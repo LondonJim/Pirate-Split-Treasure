8b7db6e0c4bde70bf0595661666626fb4cccdc62
James Sutherland <jrsutherland78@googlemail.com>
2019-02-26 20:25:17 +0000

    first mocha test, add test and splitTreasure files

---
+++ splitTreasure.js
@@ -0,0 +1,5 @@
+function splitTreasure(arr) {
+  return 3
+}
+
+module.exports = splitTreasure
--- test.js
+++ test.js
@@ -1,1 +1,17 @@
+var expect = require('chai').expect
+var splitTreasure = require('./splitTreasure')
 
+describe('splitTreasure()', () => {
+
+  it('split the array based on number of elements', () => {
+
+    let arr = [4, 4, 4]
+
+    let result = splitTreasure(arr)
+
+    expect(result).to.be.equal(3)
+  })
+
+
+
+})
