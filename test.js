3b6d89c876828734a7631c80685d43fdc9704aed
James Sutherland <jrsutherland78@googlemail.com>
2019-02-27 15:33:09 +0000

    fix naming of tests

--- test.js
+++ test.js
@@ -41,31 +41,31 @@ describe('SplitTreasure', () => {
     })
 
     // False outcome
-    it('split the array evenly 4 ways based on number and total of elements', () => {
+    it('Can not split the array evenly 4 ways based on number and total of elements', () => {
       let arr = [4, 4, 4, 4, 4]
 
       expect(splitTreasure.split(arr, 4)).to.be.equal(false)
     })
 
-    it('split the array evenly 2 ways based on number and total of elements', () => {
+    it('Can not split the array evenly 2 ways based on number and total of elements', () => {
       let arr = [27, 7, 20, 3]
 
       expect(splitTreasure.split(arr, 2)).to.be.equal(false)
     })
 
-    it('split the array evenly 2 ways based on number and total of elements', () => {
+    it('Can not split the array evenly 2 ways based on number and total of elements', () => {
       let arr = [6, 3, 2, 4, 1, 1]
 
       expect(splitTreasure.split(arr, 2)).to.be.equal(false)
     })
 
-    it('split the array evenly 4 ways based on number and total of elements', () => {
+    it('Can not split the array evenly 4 ways based on number and total of elements', () => {
       let arr = [3, 2, 7, 7, 14, 5, 3, 4, 9, 2, 1]
 
       expect(splitTreasure.split(arr, 4)).to.be.equal(false)
     })
 
-    it('split the array evenly 4 ways based on number and total of elements', () => {
+    it('Can not split the array evenly 4 ways based on number and total of elements', () => {
       let arr = [3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 
       expect(splitTreasure.split(arr, 4)).to.be.equal(false)
