cd47e2d4d029ab142fb7db1f4c33514db5b4616b
James Sutherland <jrsutherland78@googlemail.com>
2019-02-27 14:31:31 +0000

    add .SplitTreasure #split

--- splitTreasure.js
+++ splitTreasure.js
@@ -1,5 +1,67 @@
-function splitTreasure(arr) {
-  return 3
+class SplitTreasure {
+
+  split(arr, n) {
+    let piratesTotals = []
+    let pirates = []
+
+    for (let i = 0; i < n; i++) {
+      pirates.push([0])
+      piratesTotals.push(0)
+    }
+
+    arr = this._sortArr(arr)
+
+    arr.forEach((el) => {
+      let minIndex = this._findMinimumIndex(piratesTotals, el)
+      pirates[minIndex].push(el)
+      let index = 0
+      pirates.forEach((pirate) => {
+        piratesTotals[index] = pirate.reduce(this._getSum)
+        index++
+      })
+
+    })
+    return this._checkOutcome(pirates, piratesTotals)
+  }
+
+  _checkOutcome(pirates, pirateTotals) {
+    if (!!pirateTotals.reduce(function(a, b){ return (a === b) ? a : NaN; })) {
+      console.log("Even Split Possible!")
+      console.log(this._showSplit(pirates))
+      return true
+    } else {
+      console.log("Even Split not Possible!")
+      console.log(this._showSplit(pirates))
+      return false
+    }
+  }
+
+  _getSum(total, num) {
+    return total + num
+  }
+
+  _findMinimumIndex(totals, el) {
+    let index = 0
+    totals.forEach((total) => {
+      totals[index] = totals[index] + el
+      index++
+    })
+    return totals.indexOf(Math.min.apply(null, totals))
+  }
+
+  _sortArr(array) {
+    return array.sort((a, b) => b - a)
+  }
+
+  _showSplit(pirates) {
+    console.log("Pirate splits:")
+    pirates.forEach((pirate) => {
+      pirate.shift()
+      console.log(pirate)
+    })
+  }
+
 }
 
-module.exports = splitTreasure
+
+module.exports = SplitTreasure
--- test.js
+++ test.js
@@ -1,17 +1,76 @@
 var expect = require('chai').expect
-var splitTreasure = require('./splitTreasure')
+var SplitTreasure = require('./splitTreasure')
 
-describe('splitTreasure()', () => {
+describe('SplitTreasure', () => {
 
-  it('split the array based on number of elements', () => {
-
-    let arr = [4, 4, 4]
-
-    let result = splitTreasure(arr)
-
-    expect(result).to.be.equal(3)
+  before(() => {
+    splitTreasure = new SplitTreasure
   })
 
+  describe('#split', () => {
 
+    // True outcome
+    it('split the array evenly 4 ways based on number and total of elements', () => {
+      let arr = [4, 4, 4, 4]
+
+      expect(splitTreasure.split(arr, 4)).to.be.equal(true)
+    })
+
+    it('split the array evenly 2 ways based on number and total of elements', () => {
+      let arr = [27, 7, 20]
+
+      expect(splitTreasure.split(arr, 2)).to.be.equal(true)
+    })
+
+    it('split the array evenly 2 ways based on number and total of elements', () => {
+      let arr = [6, 3, 2, 4, 1]
+
+      expect(splitTreasure.split(arr, 2)).to.be.equal(true)
+    })
+
+    it('split the array evenly 4 ways based on number and total of elements', () => {
+      let arr = [3, 2, 7, 7, 14, 5, 3, 4, 9, 2]
+
+      expect(splitTreasure.split(arr, 4)).to.be.equal(true)
+    })
+
+    it('split the array evenly 4 ways based on number and total of elements', () => {
+      let arr = [3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2]
+
+      expect(splitTreasure.split(arr, 4)).to.be.equal(true)
+    })
+
+    // False outcome
+    it('split the array evenly 4 ways based on number and total of elements', () => {
+      let arr = [4, 4, 4, 4, 4]
+
+      expect(splitTreasure.split(arr, 4)).to.be.equal(false)
+    })
+
+    it('split the array evenly 2 ways based on number and total of elements', () => {
+      let arr = [27, 7, 20, 3]
+
+      expect(splitTreasure.split(arr, 2)).to.be.equal(false)
+    })
+
+    it('split the array evenly 2 ways based on number and total of elements', () => {
+      let arr = [6, 3, 2, 4, 1, 1]
+
+      expect(splitTreasure.split(arr, 2)).to.be.equal(false)
+    })
+
+    it('split the array evenly 4 ways based on number and total of elements', () => {
+      let arr = [3, 2, 7, 7, 14, 5, 3, 4, 9, 2, 1]
+
+      expect(splitTreasure.split(arr, 4)).to.be.equal(false)
+    })
+
+    it('split the array evenly 4 ways based on number and total of elements', () => {
+      let arr = [3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+
+      expect(splitTreasure.split(arr, 4)).to.be.equal(false)
+    })
+
+  })
 
 })
